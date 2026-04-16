1d440df9e876693a52c1dee6757a26d48471a7b8
aka-nse <nakase.d1220@gmail.com>
2026-04-17 00:17:30 +0900

    Merge pull request #6 from aka-nse/feature/documentation
    feature/documentation

--- src/SourceGeneratorToolkit/resources/ISourceBuilder.cs
+++ src/SourceGeneratorToolkit/resources/ISourceBuilder.cs
@@ -1,9 +1,29 @@
 namespace SourceGeneratorToolkit;
 
+/// <summary>
+/// Defines an interface for building source code with indentation and formatting support.
+/// </summary>
 internal interface ISourceBuilder
 {
+    /// <summary>
+    /// Appends a smart code part object to the source builder.
+    /// </summary>
+    /// <param name="codePart"></param>
     public void Append(CodePart codePart);
+
+    /// <summary>
+    /// Appends a line break;
+    /// </summary>
     public void AppendLine();
+
+    /// <summary>
+    /// Begins a new indented region.
+    /// </summary>
+    /// <param name="indent"></param>
     public void PushIndent(string indent);
+
+    /// <summary>
+    /// Finishes indented region which last entered.
+    /// </summary>
     public void PopIndent();
 }
